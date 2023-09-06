635066ad5561ad6ee00d8456afadc14f5eecf100
juansechaux <juansechaux@hotmail.com>
2023-09-06 15:22:46 -0700

    Prueba Monty3

--- monty.c
+++ monty.c
@@ -43,7 +43,15 @@ int main(int argc, char *argv[])
 		{
 			pall(&stack, line_number);
 		}
-		free(line); /*libera el bufer de la linea asignada*/
+		else
+		{
+			fprintf(stderr, "L%lu: unknown instruction %s\n", line_number, opcode);
+			fclose(file);
+			free(line);
+			free_dlistint(stack);
+			exit(EXIT_FAILURE);
+		}
+		free(line);
 		line = NULL;
 	}
 	free_dlistint(stack);
