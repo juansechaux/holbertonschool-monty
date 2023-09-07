a01d501edbe925f6148c8ce84331c37949948cdb
juansechaux <juansechaux@hotmail.com>
2023-09-07 09:54:20 -0700

    Task pop

--- monty.c
+++ monty.c
@@ -65,6 +65,17 @@ int main(int argc, char *argv[])
 			}
 			pint(&stack, line_number);
 		}
+		else if (strcmp(opcode, "pop") == 0)
+		{
+			if (stack == NULL)
+			{
+				fprintf(stderr, "L%lu: can't pop, stack empty\n", line_number);
+				fclose(file);
+				exit(EXIT_FAILURE);
+			}
+
+			 pop(&stack, line_number);
+		}
 		else
 		{
 			fprintf(stderr, "L%lu: unknown instruction %s\n", line_number, opcode);
@@ -129,6 +140,22 @@ void pint(stack_t **stack, unsigned int line_number)
 	(void)line_number; /*Parametro no utilizado*/
 
 	printf("%d\n", current->n);
+}
+
+/**
+ * pop - removes the first values on the stack
+ * @stack: Pointer to the stack
+ * @line_number: Line number in the script
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node_to_delete = *stack;
+	(void)line_number;/*Parametro no Utilizado*/
+
+	*stack = node_to_delete->next;
+	if (node_to_delete->next)
+		node_to_delete->next->prev = NULL;
+	free(node_to_delete);
 }
 
 /**
--- monty.h
+++ monty.h
@@ -43,5 +43,6 @@ stack_t *stack = NULL;
 void push(stack_t **stack, int value);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 void free_dlistint(stack_t *stack);
 #endif
