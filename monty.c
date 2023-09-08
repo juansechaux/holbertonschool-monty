3d024febecf2bec4b526ffa26a9e622de940c736
juansechaux <juansechaux@hotmail.com>
2023-09-08 09:36:22 -0700

    Prueba de todos los task menos betty

--- monty.c
+++ monty.c
@@ -8,9 +8,9 @@
 int main(int argc, char *argv[])
 {
 	char *opcode, *value, *line = NULL;
-	size_t line_number = 0, len = 0;
+	size_t line_number = 0, len = 0, num_of_nodes, nodes;
 	FILE *file;
-	int i;
+	int i, sum;
 
 	if (argc != 2)
 	{
@@ -78,14 +78,30 @@ int main(int argc, char *argv[])
 		}
 		else if (strcmp(opcode, "swap") == 0)
 		{
-			size_t num_of_nodes = dlistint_len(stack);
+			num_of_nodes = dlistint_len(stack);
 			if (num_of_nodes < 2)
 			{
 				fprintf(stderr, "L%lu: can't swap, stack too short\n", line_number);
 				exit(EXIT_FAILURE);
 			}
 			swap(&stack, line_number);
-		}		
+		}
+		else if (strcmp(opcode, "add") == 0)
+		{
+			nodes = dlistint_len(stack);
+			if (nodes < 2)
+			{
+				fprintf(stderr, "L%lu: can't add, stack too short\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+			sum = sum_dlistint(stack);
+			delete_dnodeint_at_index(&stack, 0);
+			stack->n = sum;
+		}
+		else if (strcmp(opcode, "nop") == 0)
+		{
+			nop(&stack, line_number);
+		}
 		else
 		{
 			fprintf(stderr, "L%lu: unknown instruction %s\n", line_number, opcode);
@@ -177,13 +193,25 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *node_to_swap = *stack;
 	(void)line_number;/*Parametro no Utilizado*/
+	int num_of_nodes = dlistint_len(*stack);
 
-	*stack = node_to_swap->next;
-	(*stack)->prev = NULL;
-	node_to_swap->prev = (*stack)->next->prev;
-	(*stack)->next->prev = node_to_swap;
-	node_to_swap->next = (*stack)->next;
-	(*stack)->next = node_to_swap;
+	if (num_of_nodes >=3)
+	{
+		*stack = node_to_swap->next;
+		(*stack)->prev = NULL;
+		node_to_swap->prev = (*stack)->next->prev;
+		(*stack)->next->prev = node_to_swap;
+		node_to_swap->next = (*stack)->next;
+		(*stack)->next = node_to_swap;
+	}
+	else
+	{
+		*stack = node_to_swap->next;
+		(*stack)->prev = NULL;
+		node_to_swap->prev = node_to_swap->next;
+		(*stack)->next = node_to_swap;
+		node_to_swap->next = NULL;
+	}
 
 }
 
@@ -219,4 +247,78 @@ size_t dlistint_len(const stack_t *stack)
 		i++;
 	}
 	return (i);
+}
+/**
+ * sum_dlistint - function that adds the top two elements of the stack
+ * @stack : pointer to list
+ * Return: sum of top two elements or the stack
+ */
+
+int sum_dlistint(stack_t *stack)
+
+{
+	int sum = 0, i = 0;
+
+	stack_t *current = stack;
+
+	while (i < 2)
+	{
+		sum += current->n;
+		current = current->next;
+		i++;
+	}
+	return (sum);
+}
+
+/**
+ * delete_dnodeint_at_index - func that delet a new node at a given position
+ * @head: pointer to the header of the nodes
+ * @index: the index of the list where the new node should be added
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_dnodeint_at_index(stack_t **stack, unsigned int index)
+{
+	unsigned int i;
+	stack_t *node_to_delete;
+
+	if (!stack || !*stack)
+		return (-1);
+	node_to_delete = *stack;
+
+	if (index == 0)
+	{
+		*stack = node_to_delete->next;
+		if (node_to_delete->next)
+			node_to_delete->next->prev = NULL;
+		free(node_to_delete);
+		return (1);
+	}
+	for (i = 0; node_to_delete && i < index; i++)
+	{
+		node_to_delete = node_to_delete->next;
+	}
+	if (!node_to_delete)
+		return (-1);
+	if (!node_to_delete->next)
+	{
+		node_to_delete->prev->next = NULL;
+		free(node_to_delete);
+		return (1);
+	}
+	node_to_delete->prev->next = node_to_delete->next;
+	node_to_delete->next->prev = node_to_delete->prev;
+	free(node_to_delete);
+	return (1);
+}
+/**
+ *nop- Function that doesn´t do anything
+ *@stack: stack struct
+ *@line_number: line_number
+ *Return: no return
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
 }
